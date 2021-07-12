8b61974414e0d8dd91b80c9f2c69e60a6102ae62
benjamin <benjamin.olmsted@gmail.com>
2021-07-12 16:12:43 -0700

    more fetches

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
+/*These are our endpoints for our fetches
+https://www.coingecko.com/api/documentations/v3
+*/
 const baseURL = 'https://api.coingecko.com/api/v3'
 const trendingEndpoint = '/search/trending'
 const coinsEndpoint = '/coins/'
+const listEndpoint = '/coins/list'
 
+/* document selectors */
 const body = document.querySelector('body')
 
+/*entry point to app*/
+getTrending()
+GetList()
 
-fetch(baseURL+trendingEndpoint)
-.then(resp => resp.json())
-.then(json => renderTrending(json))
+/*fetches*/
 
+function getTrending(){
+    fetch(baseURL+trendingEndpoint)
+    .then(resp => resp.json())
+    .then(json => renderTrending(json))
+}
+
+function GetList(){
+    fetch(baseURL + listEndpoint)
+    .then(resp => resp.json())
+    .then(json => {
+        console.log(json.sort( (a,b) => {
+            return a.coingecko_rank -b.coingecko_rank
+        }))
+    })
+}
+
+function getCoinDetails(id){
+    fetch(baseURL + coinsEndpoint + id)
+    .then(resp => resp.json())
+    .then(renderFullCoin)
+}
+
+/*  Render functions */
 
 function renderTrending(trendingJSON){
     console.log(trendingJSON)
-    trendingJSON.coins.forEach(coin => renderCoin(coin.item))
+    trendingJSON.coins.forEach(coin => renderTrendingCoin(coin.item))
 }
 
-function renderCoin(coin){
+function renderFullCoin(coin){
     console.log(coin)
+}
+
+function renderTrendingCoin(coin){
     let div = document.createElement('div')
     let h1Name = document.createElement('h1')
     let h2Price = document.createElement('h2')
     let img = document.createElement('img')
     h1Name.textContent = coin.name
-    h2Price = coin.price_btc
+    //h2Price = coin.price_btc
     img.src = coin.small
 
     div.dataset.id = coin.id
@@ -31,10 +63,15 @@ function renderCoin(coin){
     })
     div.append(h1Name, h2Price, img)
     body.append(div)
+    
+    fetch(baseURL + coinsEndpoint + coin.id)
+    .then(resp => resp.json())
+    .then((json) => {
+        console.log(json.market_data.price_change_percentage_24h)
+        h2Price.textContent = json.market_data.price_change_percentage_24h
+
+    })
 }
 
-function getCoinDetails(id){
-    fetch(baseURL + coinsEndpoint + id)
-    .then(resp => resp.json())
-    .then(console.log)
-}
+
+
